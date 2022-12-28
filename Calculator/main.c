6490ce627813aa32b02d7938f03c90d10de02c00
gh3ma <gaes.sy.2000@gmail.com>
2022-12-28 19:26:38 +0300

    1.

--- Calculator/main.c
+++ Calculator/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
+
 int main() {
-    printf("Hello, World!\n");
+   //float addition ,subtraction ,multiplication ,division ;
+   int x ;
+   float a ,b ,c ;
+    printf("Enter a & b numbers:\n");
+    scanf("%f\n%f" , &a ,&b);
+    printf("Enter the number of operation you want calculate it:\n");
+    printf("1- addition:\n");
+    printf("2- subtraction:\n");
+    printf("3- multiplication:\n");
+    printf("4- division:\n");
+    scanf("%d", &x);
+    switch (x) {
+        case 1:
+        c = a + b ;
+            printf("%.2f" ,c);
+            break;
+        case 2:
+            c = a - b ;
+            printf("%.2f" ,c);
+            break;
+        case 3:
+            c = a * b ;
+            printf("%.2f" ,c);
+            break;
+        case 4:
+            c = a / b ;
+            printf("%.2f" ,c);
+            break;
+    }
     return 0;
 }
